fc2ddf0359f1351f8f0686fc3683a4876908d71d
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2018-10-18 12:48:38 +0200

    added compression to enable gzip

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var compression = require('compression');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(compression());
 app.use(express.static('public'));
-app.use(compression);
 
 app.get('/', function (req, res) {
   res.send('Gabriel\'s Mayta Assets');
